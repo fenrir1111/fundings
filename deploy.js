89ae009cece5cf88bca72281b4483df5f338ecae
chenhao-bj@youedata.com <chenhao-bj@youedata.com>
2018-11-21 18:51:11 +0800

    '20181121'

--- deploy.js
+++ deploy.js
@@ -15,20 +15,25 @@ deployFactory = async () => {
     const bytecode = '0x'+contract.bytecode
     const gas = await web3.eth.estimateGas({data:bytecode})
 
-    // 0x8024AFdBDDa10D032D2f35dE6829aAB3Dd740cB6
+    // 0xa9ca16574Ed47532A2F166A8974959046a4Bbc07
     const contractResult = await new web3.eth.Contract(JSON.parse(contract.interface))
         .deploy({data:bytecode})
         .send({
             from:accounts[0],
             gas:gas
         })
-    // console.log(accounts[0])
-    console.log(contractResult)
+    console.log(accounts[0])
+    console.log(contractResult.options.address)
+    console.log('================== FundingFactory interface')
+    console.log(contract.interface)
 
 
 }
-deploy = () =>{
-   deployFactory()
+deploy = async () =>{
+  await deployFactory()
+    console.log('================== Funding interface')
+    console.log(compileContracts.Funding.interface)
+
 }
 
 deploy()
